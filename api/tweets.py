dad87c541319298c05fc77097fd29e35aa3aa524
projetormb <projetormb@gmail.com>
2017-03-16 15:50:49 -0300

    ajustes da api

--- api/tweets.py
+++ api/tweets.py
@@ -6,8 +6,6 @@ app = Flask(__name__, template_folder='./templates', static_url_path='/static')
 
 @app.route('/')
 def home():
-    #return "oi mundo eu sou o flask", 200
-
     dados = {}
     dados['cabec3'] = 'cabecalho 3 teste em flask'
 
@@ -15,14 +13,18 @@ def home():
     usuarios.append({ 'userID' : 'rmbertoni', 'userName' : 'Rafael Malta Bertoni'})
     usuarios.append({ 'userID' : 'lupcoelho', 'userName' : 'Luciana P. Coelho'})
 
-
-
-
     return render_template("index.html", dados=dados, usuarios=usuarios), 200
 
-@app.route('/api')
+@app.route('/api', methods=['POST'])
 def api():
-    return "retorno da api em flask", 200
+    #return "retorno da api em flask", 200
+    retorno = {}
+    retorno[0] = 'tweet 1'
+    retorno[1] = 'este e o tweet 2'
+    retorno[2] = 'tweet numero 3'
+
+    from flask import jsonify
+    return jsonify(retorno)
 
 
 app.run()
